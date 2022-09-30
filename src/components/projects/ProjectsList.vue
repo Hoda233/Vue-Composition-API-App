da2ba61411357a691bf3d40623189da06fb734e5
HodaGamal <77369927+Hoda233@users.noreply.github.com>
2022-09-30 20:49:40 +0200

    Add files via upload

---
+++ src/components/projects/ProjectsList.vue
@@ -0,0 +1,93 @@
+<template>
+  <base-container v-if="user">
+    <h2>{{ user.fullName }}: Projects</h2>
+    <base-search v-if="hasProjects" @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
+    <ul v-if="hasProjects">
+      <project-item v-for="prj in availableProjects" :key="prj.id" :title="prj.title"></project-item>
+    </ul>
+    <h3 v-else>No projects found.</h3>
+  </base-container>
+  <base-container v-else>
+    <h3>No user selected.</h3>
+  </base-container>
+</template>
+
+<script>
+  import { computed,watch, toRefs} from 'vue'
+  import useSearch from '../../hooks/search';
+import ProjectItem from './ProjectItem.vue';
+
+export default {
+  components: {
+    ProjectItem,
+  },
+  props: ['user'],
+  setup(props){
+
+    const {user}=toRefs(props);
+
+    const projects = computed(function(){
+      return user.value ? user.value.projects : [];
+    });
+    
+    const { enteredSearchTerm, availableItems, updateSearch } = useSearch(
+      projects,
+      'title'
+    );
+
+    // const enteredSearchTerm=ref('');
+    // const activeSearchTerm=ref('');
+
+    // const availableProjects=computed(function(){
+    //       if (activeSearchTerm.value) {
+    //     return props.user.projects.filter((prj) =>
+    //       prj.title.includes(activeSearchTerm.value)
+    //     );
+    //   }
+    //   return this.user.projects;
+    // });
+
+    const hasProjects=computed(function(){
+      return user.value.projects && availableItems.value.length > 0;
+    });
+
+    //   function updateSearch(val) {
+    //   enteredSearchTerm.value = val;
+    // }
+
+    // watch(enteredSearchTerm,function(newValue){
+    //       setTimeout(() => {
+    //     if (newValue === enteredSearchTerm.value) {
+    //       activeSearchTerm.value = newValue;
+    //     }
+    //   }, 300);
+    // });
+
+    // const propsWithRefs=toRefs(props);
+    // const user=propsWithRefs.user;
+
+    
+
+    watch(user,function(){
+      updateSearch('');
+    });
+
+    return {
+      enteredSearchTerm,
+      availableProjects:availableItems,
+      hasProjects,
+      updateSearch,
+
+    };
+  }
+ 
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
